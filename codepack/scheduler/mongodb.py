b6198b5cb479675dc216d739123215e359a51363
ihnokim <ihnokim58@gmail.com>
2021-07-02 01:14:04 +0900

    Add codepack information into jobstore and implement codepack scheduling

--- codepack/scheduler/mongodb.py
+++ codepack/scheduler/mongodb.py
@@ -1,6 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from codepack.scheduler.jobstores.mongodb import MongoDBJobStore
 from codepack.interface import MongoDB
+from codepack import CodePack
 
 
 class MongoScheduler:
@@ -30,8 +31,27 @@ class MongoScheduler:
         self.scheduler.shutdown()
         self.mongodb.close()
 
-    def add_job(self, func, id, trigger, jobstore='mongo', **kwargs):
-        return self.scheduler.add_job(func=func, id=id, trigger=trigger, jobstore=jobstore, **kwargs)
+    def add_job(self, func, job_id, trigger, jobstore='mongo', **kwargs):
+        return self.scheduler.add_job(func=func, id=job_id, trigger=trigger, jobstore=jobstore, **kwargs)
+
+    def add_codepack(self, codepack, trigger, job_id=None, arg_dict=None, lazy=False, jobstore='mongo', **kwargs):
+        if arg_dict is None:
+            arg_dict = codepack.make_arg_dict()
+        if job_id is None:
+            job_id = codepack.id
+        ret = self.add_job(self.run_codepack, job_id=job_id, trigger=trigger, jobstore=jobstore,
+                           kwargs={'codepack': codepack, 'arg_dict': arg_dict, 'lazy': lazy}, **kwargs)
+        self.mongodb[self.db][self.collection].update_one({'_id': job_id},
+                                                          {'$set': {'codepack': codepack.id, 'arg_dict': arg_dict}})
+        return ret
+
+    def add_codepack_from_db(self, id, db, collection, trigger, config, ssh_config=None, job_id=None, arg_dict=None, lazy=False, jobstore='mongo', **kwargs):
+        codepack = CodePack.from_db(id=id, db=db, collection=collection, config=config, ssh_config=ssh_config)
+        return self.add_codepack(codepack=codepack, trigger=trigger, job_id=job_id, arg_dict=arg_dict, lazy=lazy, jobstore=jobstore, **kwargs)
 
     def remove_job(self, id, **kwargs):
         return self.scheduler.remove_job(job_id=id, **kwargs)
+
+    @staticmethod
+    def run_codepack(codepack, arg_dict=None, lazy=False):
+        return codepack(arg_dict=arg_dict, lazy=lazy)
