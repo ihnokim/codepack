a64a6f801d2ed5aa13bfc70ed4b226b6440410e7
ihnokim <ihnokim58@gmail.com>
2021-10-24 22:29:56 +0900

    Implement get_config

--- codepack/utils/__init__.py
+++ codepack/utils/__init__.py
@@ -1,1 +1,9 @@
 from codepack.utils.looper import Looper
+from configparser import ConfigParser
+
+
+def get_config(filename, section):
+    cp = ConfigParser()
+    cp.read(filename)
+    items = cp.items(section)
+    return {item[0]: item[1] for item in items}
