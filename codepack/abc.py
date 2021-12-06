1685f0e008b609a2e70315c65acfc9b6f3334552
ihnokim <ihnokim58@gmail.com>
2021-12-07 00:56:21 +0900

    Remove dependency on MongoDB, import json_util to process datetime instances, and add serial_number as an optional argument to __init__

--- codepack/abc.py
+++ codepack/abc.py
@@ -2,25 +2,27 @@ import abc
 import bson
 import json
 import dill
-import os
-from codepack.interface import MongoDB
-from codepack.utils import get_config
 from datetime import datetime
+from bson import json_util
 
 
 class Storable(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def __init__(self):
+    def __init__(self, id=None, serial_number=None):
         """initialize an instance"""
-        self.id = None
+        self.id = id
+        self.serial_number = serial_number if serial_number else self.generate_serial_number()
+
+    def generate_serial_number(self):
+        return (str(id(self)) + str(datetime.now().timestamp())).replace('.', '')
 
     def to_json(self):
-        return json.dumps(self.to_dict())
+        return json.dumps(self.to_dict(), default=json_util.default)
 
     @classmethod
-    def from_json(cls, j):
-        d = json.loads(j)
-        return cls.from_dict(d)
+    def from_json(cls, j, *args, **kwargs):
+        d = json.loads(j, object_hook=json_util.object_hook)
+        return cls.from_dict(d, *args, **kwargs)
 
     def to_binary(self):
         return bson.Binary(dill.dumps(self))
@@ -29,138 +31,48 @@ class Storable(metaclass=abc.ABCMeta):
     def from_binary(cls, b):
         return dill.loads(b)
 
-    def to_file(self, filename):
-        with open(filename, 'w') as f:
+    def to_file(self, path):
+        with open(path, 'w') as f:
             f.write(self.to_json())
 
     @classmethod
-    def from_file(cls, filename):
-        with open(filename, 'r') as f:
-            ret = cls.from_json(f.read())
+    def from_file(cls, path, *args, **kwargs):
+        with open(path, 'r') as f:
+            ret = cls.from_json(f.read(), *args, **kwargs)
         return ret
 
-    def to_db(self, db=None, collection=None, config=None, ssh_config=None, mongodb=None, **kwargs):
-        mongodb, db, collection, new_connection = MongoDBService.get_mongodb(section=self.__class__.__name__.lower(),
-                                                                             conn_config=config,
-                                                                             ssh_config=ssh_config,
-                                                                             db=db, collection=collection,
-                                                                             mongodb=mongodb, **kwargs)
-        d = self.to_dict()
-        try:
-            mongodb[db][collection].insert_one(d)
-        finally:
-            if new_connection:
-                mongodb.close()
+    def to_db(self, mongodb, db, collection, *args, **kwargs):
+        mongodb[db][collection].insert_one(self.to_dict(*args, **kwargs))
 
     @classmethod
-    def from_db(cls, id, db=None, collection=None, config=None, ssh_config=None, mongodb=None, **kwargs):
-        mongodb, db, collection, new_connection = MongoDBService.get_mongodb(section=cls.__name__.lower(),
-                                                                             conn_config=config,
-                                                                             ssh_config=ssh_config,
-                                                                             db=db, collection=collection,
-                                                                             mongodb=mongodb, **kwargs)
-        try:
-            d = mongodb[db][collection].find_one({'_id': id})
-        finally:
-            if new_connection:
-                mongodb.close()
+    def from_db(cls, id, mongodb, db, collection, *args, **kwargs):
+        d = mongodb[db][collection].find_one({'_id': id})
         if d is None:
             return d
         else:
-            return cls.from_dict(d)
-
-    @classmethod
-    def _config_assertion_error_message(cls, x):
-        return "'%s' should be given as an argument or defined in a configuration file in 'config_filepath' " \
-               "or os.environ['CODEPACK_CONFIG_FILEPATH']" % x
-
-    @classmethod
-    def get_db_info(cls, section=None, config_filepath=None, conn_config=None, db=None, collection=None):
-        if not config_filepath:
-            config_filepath = os.environ.get('CODEPACK_CONFIG_FILEPATH', None)
-        if config_filepath:
-            if not conn_config or not db or not collection:
-                config = get_config(config_filepath, section=section)
-                if not conn_config:
-                    if config['source'] not in ['mongodb']:
-                        raise NotImplementedError("'%s' is unknown data source")
-                    conn_config_filepath = get_config(config_filepath, section='conn')['filepath']
-                    conn_config = get_config(conn_config_filepath, section=config['source'])
-                if not db:
-                    db = config['db']
-                if not collection:
-                    collection = config['collection']
-        else:
-            for k, v in {'db': db, 'collection': collection}.items():
-                if not v:
-                    raise AssertionError(cls._config_assertion_error_message(k))
-        return conn_config, db, collection
+            return cls.from_dict(d, *args, **kwargs)
 
     @abc.abstractmethod
-    def to_dict(self):
-        pass
+    def to_dict(self, *args, **kwargs):
+        """save to dict"""
 
     @classmethod
     @abc.abstractmethod
-    def from_dict(cls, d):
-        pass
+    def from_dict(cls, d, *args, **kwargs):
+        """load from dict"""
+
+    @classmethod
+    def get_path(cls, serial_number, path='./'):
+        return '%s%s.json' % (path, serial_number)
 
 
 class CodeBase(Storable, metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def __init__(self, serial_number=None):
-        super().__init__()
-        self.serial_number = serial_number if serial_number else self.generate_serial_number()
-
-    def generate_serial_number(self):
-        return (str(id(self)) + str(datetime.now().timestamp())).replace('.', '')
+    def __init__(self, id=None, serial_number=None):
+        super().__init__(id=id, serial_number=serial_number)
 
 
 class CodePackBase(Storable, metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def __init__(self):
-        super().__init__()
-
-
-class MongoDBService:
-    def __init__(self, section=None, config_filepath=None, db=None, collection=None, conn_config=None, ssh_config=None,
-                 mongodb=None, online=False, **kwargs):
-        self.mongodb= None
-        self.db = None
-        self.collection = None
-        self.new_connection = None
-        self.online = False
-        self.link_to_mongodb(section=section, config_filepath=config_filepath,
-                             db=db, collection=collection,
-                             conn_config=conn_config, ssh_config=ssh_config,
-                             mongodb=mongodb, online=online, **kwargs)
-
-    def link_to_mongodb(self, section=None, config_filepath=None, db=None, collection=None,
-                        conn_config=None, ssh_config=None,
-                        mongodb=None, online=False, **kwargs):
-        new_connection = False
-        self.online = online
-        if self.online:
-            mongodb, db, collection, new_connection = self.get_mongodb(section=section, config_filepath=config_filepath,
-                                                                       db=db, collection=collection,
-                                                                       conn_config=conn_config, ssh_config=ssh_config,
-                                                                       mongodb=mongodb, **kwargs)
-        self.mongodb = mongodb
-        self.db = db
-        self.collection = collection
-        self.new_connection = new_connection
-
-    @classmethod
-    def get_mongodb(cls, section=None, config_filepath=None, conn_config=None, ssh_config=None,
-                    db=None, collection=None, mongodb=None, **kwargs):
-        new_connection = False
-        if not mongodb:
-            conn_config, db, collection = Storable.get_db_info(section=section, config_filepath=config_filepath,
-                                                               conn_config=conn_config, db=db, collection=collection)
-            if conn_config:
-                mongodb = MongoDB(config=conn_config, ssh_config=ssh_config, **kwargs)
-                new_connection = True
-        else:
-            _, db, collection = Storable.get_db_info(section=section, config_filepath=config_filepath,
-                                                     conn_config=mongodb.config, db=db, collection=collection)
-        return mongodb, db, collection, new_connection
+    def __init__(self, id=None, serial_number=None):
+        super().__init__(id=id, serial_number=serial_number)
