aa19264f0226e1efb812b5a7fb714866919e6bf6
ihnokim <ihnokim58@gmail.com>
2021-12-31 08:24:16 +0900

    Implement DependencyState

---
+++ codepack/utils/dependency_state.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+
+class DependencyState(Enum):
+    READY = 0
+    NOT_READY = 1
+    ERROR = 2
+
+    def __str__(self):
+        return self.name  # pragma: no cover
+
+    def __repr__(self):
+        return self.__str__()  # pragma: no cover
+
+    def __eq__(self, other):
+        if isinstance(other, type(self)):
+            return self.name == other.name
+        elif isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, int):
+            return self.name == self.__class__(other).name
+        else:
+            return False  # pragma: no cover
