bf5285b7041ec50246a5a0c8f70acdb408f80482
ihnokim <ihnokim58@gmail.com>
2022-01-10 08:17:11 +0900

    Add test for CodePackSnapshot

--- tests/test_snapshot.py
+++ tests/test_snapshot.py
@@ -1,8 +1,9 @@
-from codepack.utils.snapshot import Snapshot
-from codepack.utils.snapshot import CodeSnapshot
-from codepack import Code
+from codepack.snapshot import Snapshot
+from codepack.snapshot import CodeSnapshot
+from codepack import Code, CodePack
 from tests import *
 from datetime import datetime
+import pytest
 
 
 def test_snapshot_to_dict_and_from_dict():
@@ -86,3 +87,68 @@ def test_code_to_snapshot_and_from_snapshot(default_os_env):
     code2 = Code.from_snapshot(snapshot=snapshot1)
     assert code2.get_state() == 'UNKNOWN'
     assert code2(1, 2) == 3
+
+
+def test_codepack_to_snapshot_and_from_snapshot(default_os_env):
+    c1 = Code(add3)
+    c2 = Code(mul2)
+    c3 = Code(combination)
+    c4 = Code(linear)
+    c5 = Code(print_x)
+
+    c1 >> c3
+    c2 >> c3
+    c3 >> [c4, c5]
+
+    c3.receive('c') << c1
+    c4.receive('c') << c3
+    c5.receive('x') << c3
+
+    cp1 = CodePack(id='test_codepack', code=c1, subscribe=c4)
+
+    argpack = cp1.make_argpack()
+    argpack['add3']['a'] = 1
+    argpack['add3']['b'] = 2
+    argpack['add3']['c'] = 3
+    argpack['mul2']['a'] = 2
+    argpack['mul2']['b'] = 1
+    argpack['combination']['a'] = 2
+    argpack['combination']['b'] = 5
+    argpack['linear']['b'] = 7
+    argpack['linear']['a'] = 5
+
+    ret = None
+    with pytest.raises(TypeError):
+        ret = cp1(argpack)
+
+    assert ret is None
+    assert cp1.get_state() == 'ERROR'
+    snapshot1 = cp1.to_snapshot(argpack=argpack)
+    cp2 = CodePack.from_snapshot(snapshot1)
+    assert cp1.id == cp2.id
+    assert cp1.serial_number == cp2.serial_number
+    assert cp1.get_state() == cp2.get_state()
+    cp1_source = cp1.get_source()
+    cp2_source = cp2.get_source()
+    assert cp1_source.keys() == cp2_source.keys()
+    for id in cp1_source.keys():
+        assert cp1_source[id].strip() == cp2_source[id].strip()
+    # assert cp1.get_structure() == cp2.get_structure()
+    assert cp1.subscribe == cp2.subscribe
+    assert set(cp1.codes.keys()) == set(cp2.codes.keys())
+    for code_id in cp1.codes.keys():
+        code1 = cp1.codes[code_id]
+        code2 = cp2.codes[code_id]
+        assert code1.serial_number == code2.serial_number
+        assert code1.parents.keys() == code2.parents.keys()
+        for id in code1.parents.keys():
+            assert code1.parents[id].serial_number == code2.parents[id].serial_number
+        assert code1.children.keys() == code2.children.keys()
+        for id in code1.children.keys():
+            assert code1.children[id].serial_number == code2.children[id].serial_number
+        assert code1.get_state() == code2.get_state()
+        assert code1.dependency.keys() == code2.dependency.keys()
+        for serial_number in code1.dependency.keys():
+            assert code1.dependency[serial_number].id == code2.dependency[serial_number].id
+            assert code1.dependency[serial_number].serial_number == code2.dependency[serial_number].serial_number
+            assert code1.dependency[serial_number].arg == code2.dependency[serial_number].arg
