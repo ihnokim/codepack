614b08ba962ef92f3b6c0236efe611c1d6c78f72
ihnokim <ihnokim58@gmail.com>
2022-01-12 02:01:44 +0900

    Add test for Dependency

---
+++ tests/test_dependency.py
@@ -0,0 +1,12 @@
+from codepack.dependency import Dependency
+
+
+def test_eq():
+    dependency1 = Dependency(arg='a', id='add3', serial_number='1234')
+    dependency2 = Dependency(arg='a', id='add3', serial_number='1234')
+    assert dependency1 == dependency2
+    dependency_dict2 = dependency2.to_dict()
+    assert dependency1 == dependency_dict2
+    dependency2.arg = 'b'
+    assert dependency1 != dependency2
+    assert dependency2 != dependency_dict2
