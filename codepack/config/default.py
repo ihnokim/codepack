a50fd26ed1ab5c3aca1f5058ade7758e1b90bf37
ihnokim <ihnokim58@gmail.com>
2022-02-10 01:34:22 +0900

    Implement _get_storage_config, get_scheduler, and get_employee

--- codepack/config/default.py
+++ codepack/config/default.py
@@ -1,5 +1,7 @@
 from codepack.config.config import Config
 from codepack.config.alias import Alias
+import os
+from inspect import getfullargspec
 
 
 class Default:
@@ -24,6 +26,17 @@ class Default:
     @staticmethod
     def _get_config(config_path: str = None):
         return Config(config_path=config_path)
+
+    @classmethod
+    def _get_storage_config(cls, section: str, config_path: str = None):
+        if config_path:
+            _config = cls._get_config(config_path=config_path)
+        elif not cls.config:
+            cls.init_config()
+            _config = cls.config
+        else:
+            _config = cls.config
+        return _config.get_storage_config(section=section)
 
     @classmethod
     def init_alias(cls, config_path: str = None, alias_path: str = None):
@@ -77,19 +90,57 @@ class Default:
     def get_storage_instance(cls, section: str, instance_type: str, config_path: str = None, alias_path: str = None):
         key = '%s_%s' % (section, instance_type)
         if config_path or alias_path or key not in cls.instances:
-            if config_path:
-                _config = cls._get_config(config_path=config_path)
-            elif not cls.config:
-                cls.init_config()
-                _config = cls.config
-            else:
-                _config = cls.config
-            storage_config = _config.get_storage_config(section=section)
+            storage_config = cls._get_storage_config(section=section, config_path=config_path)
             source = storage_config.pop('source')
             alias = cls.get_alias_from_source(source=source, suffix=instance_type)
             c = cls.get_class_from_alias(alias, config_path=config_path, alias_path=alias_path)
             item_type = cls.get_class_from_alias(section, config_path=config_path, alias_path=alias_path)
             _instance = c(item_type=item_type, **storage_config)
+            if config_path is None and alias_path is None:
+                cls.instances[key] = _instance
+            return _instance
+        else:
+            return cls.instances[key]
+
+    @classmethod
+    def get_scheduler(cls, section: str = 'scheduler', config_path: str = None, alias_path: str = None):
+        key = section
+        if config_path or alias_path or key not in cls.instances:
+            storage_config = cls._get_storage_config(section=section, config_path=config_path)
+            source = storage_config.pop('source')
+            alias = cls.get_alias_from_source(source=source, suffix=section)
+            c = cls.get_class_from_alias(alias, config_path=config_path, alias_path=alias_path)
+            if 'CODEPACK_SCHEDULER_SUPERVISOR' in os.environ:
+                storage_config['supervisor'] = os.environ['CODEPACK_SCHEDULER_SUPERVISOR']
+            _instance = c(**storage_config)
+            if config_path is None and alias_path is None:
+                cls.instances[key] = _instance
+            return _instance
+        else:
+            return cls.instances[key]
+
+    @classmethod
+    def get_employee(cls, section: str, config_path: str = None, alias_path: str = None):
+        key = section
+        if 'worker' in section:
+            kafka_type = 'consumer'
+        elif 'supervisor' in section:
+            kafka_type = 'producer'
+        else:
+            raise NotImplementedError("'%s' is unknown")
+        if config_path or alias_path or key not in cls.instances:
+            storage_config = cls._get_storage_config(section=section, config_path=config_path)
+            storage_config.pop('source')
+            kafka_config = storage_config.pop('kafka')
+            c = cls.get_class_from_alias(section, config_path=config_path, alias_path=alias_path)
+            employee_config = dict()
+            for k, v in storage_config.items():
+                if k in getfullargspec(c.__init__).args:
+                    employee_config[k] = v
+                else:
+                    kafka_config[k] = v
+            employee_config[kafka_type] = kafka_config
+            _instance = c(**employee_config)
             if config_path is None and alias_path is None:
                 cls.instances[key] = _instance
             return _instance
