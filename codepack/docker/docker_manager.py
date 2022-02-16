f9b57bc06e8311ec4c459f9ae92d5214fc8a2fc2
ihnokim <ihnokim58@gmail.com>
2022-02-16 10:13:10 +0900

    Refactor DockerManager

--- codepack/docker/docker_manager.py
+++ codepack/docker/docker_manager.py
@@ -2,14 +2,19 @@ import os
 from docker import from_env
 from docker.errors import ImageNotFound
 from shutil import rmtree
+from typing import Union
+import codepack.interface
+from docker import DockerClient
 
 
 class DockerManager:
-    def __init__(self, docker, path='./'):
+    CONTAINER_WORKDIR = '/usr/src/codepack'
+
+    def __init__(self, docker: Union[codepack.interface.Docker, DockerClient], path: str = './'):
         self.docker = docker
         self.path = path
 
-    def get_image(self, image):
+    def get_image(self, image: str):
         self.docker = from_env()
         ret = None
         try:
@@ -19,18 +24,79 @@ class DockerManager:
         finally:
             return ret
 
-    def pull_image(self, image):
+    def pull_image(self, image: str):
         return self.docker.images.pull(image)
 
-    def push_image(self, image):
+    def push_image(self, image: str):
         return self.docker.images.push(image)
 
-    def run(self, image, command=None):
-        return self.docker.containers.run(image=image, volumes=['%s:/usr/src/codepack' % os.path.abspath(self.path)],
-                                          working_dir='/usr/src/codepack', auto_remove=True, name=id(self),
-                                          command=command)
+    def run(self, image: str, **kwargs):
+        return self.docker.containers.run(image=image, volumes=['%s:%s' % (os.path.abspath(self.path), self.CONTAINER_WORKDIR)],
+                                          working_dir=self.CONTAINER_WORKDIR, auto_remove=True, name=id(self), **kwargs)
 
-    def build_image(self, tag, base_image, args=None, envs=None, requirements=None, tmp_dir=None):
+    @staticmethod
+    def _collect_requirements_in_dict(r: list, d: dict):
+        for _r in r:
+            tokens = _r.split('==')
+            if len(tokens) == 2:
+                d[tokens[0]] = tokens[1]
+            elif len(tokens) == 1:
+                d[tokens[0]] = None
+            else:
+                raise IndexError(len(tokens))
+
+    @staticmethod
+    def combine_requirements(base: list, additive: list):
+        ret = list()
+        buffer = dict()
+        DockerManager._collect_requirements_in_dict(base, buffer)
+        DockerManager._collect_requirements_in_dict(additive, buffer)
+        for module, version in buffer.items():
+            tmp = module
+            if version:
+                tmp += '==%s' % version
+            ret.append(tmp)
+        return ret
+
+    @staticmethod
+    def make_dockerfile(path: str, base_image: str, args: dict = None, envs: dict = None,
+                        requirements: list = None, pip_options: dict = None):
+        ret = str()
+        if args is None:
+            args = dict()
+        if envs is None:
+            envs = dict()
+        if requirements is None:
+            requirements = list()
+        lines = list()
+        lines.append('FROM %s' % base_image)
+        for k, v in args.items():
+            lines.append('ARG %s %s' % (k, v))
+        for k, v in envs.items():
+            lines.append('ENV %s %s' % (k, v))
+        if len(requirements) > 0:
+            prefix = 'RUN python -m pip install'
+            if pip_options:
+                for k, v in pip_options.items():
+                    if v:
+                        if isinstance(v, list):
+                            for _v in v:
+                                prefix += ' --%s %s' % (k, _v)
+                        elif isinstance(v, str):
+                            prefix += ' --%s %s' % (k, v)
+                        else:
+                            raise TypeError(type(v))
+                    else:
+                        prefix += ' --%s' % k
+            lines.append('%s pip' % prefix)
+            lines.append('%s %s' % (prefix, ' '.join(requirements)))
+        with open(os.path.join(path, 'Dockerfile'), 'w') as f:
+            f.writelines(line + '\n' for line in lines)
+        for line in lines:
+            ret += line + '\n'
+        return ret
+
+    def build_image(self, tag, base_image, args=None, envs=None, requirements=None, pip_options=None, tmp_dir=None):
         if not tmp_dir:
             tmp_dir = str(id(self))
         path = os.path.join(self.path, tmp_dir)
@@ -40,29 +106,11 @@ class DockerManager:
         try:
             if self.get_image(base_image) is None:
                 return None, None
-            if args is None:
-                args = dict()
-            if envs is None:
-                envs = dict()
             if not os.path.exists(path):
                 os.makedirs(path)
                 new_path = True
-            if requirements is None:
-                requirements = list()
-            if len(requirements) > 0:
-                with open(os.path.join(path, 'requirements.txt'), 'w') as f:
-                    lines = list()
-                    lines.append('FROM %s' % base_image)
-                    lines.append('COPY requirements.txt requirements.txt')
-                    for k, v in args.items():
-                        lines.append('ARG %s %s' % (k, v))
-                    for k, v in envs.items():
-                        lines.append('ENV %s %s' % (k, v))
-                    if len(requirements) > 0:
-                        prefix = 'RUN python -m pip install --upgrade --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org'
-                        lines.append('%s pip' % prefix)
-                        lines.append('%s -r requirements.txt' % prefix)
-                    f.writelines(line + '\n' for line in lines)
+            self.make_dockerfile(path, base_image=base_image, args=args, envs=envs,
+                                 requirements=requirements, pip_options=pip_options)
             image, logs = self.docker.images.build(path=path, tag=tag)
         finally:
             if new_path:
---
+++ tests/test_docker_manager.py
@@ -0,0 +1,19 @@
+from codepack.docker import DockerManager
+
+
+def test_make_dockerfile():
+    ret = DockerManager.make_dockerfile(path='docker_test', base_image='python:3.7-slim',
+                                        args={'http_proxy': "http://1.2.3.4:5678", 'https_proxy': "''"},
+                                        requirements=['pymongo==3.12.1', 'numpy'],
+                                        pip_options={'upgrade': None,
+                                                     'trusted-host': ['pypi.python.org', 'pypi.org', 'files.pythonhosted.org']})
+    assert ret == "FROM python:3.7-slim\nARG http_proxy http://1.2.3.4:5678\nARG https_proxy ''\nRUN python -m " \
+                  "pip install --upgrade --trusted-host pypi.python.org --trusted-host pypi.org " \
+                  "--trusted-host files.pythonhosted.org pip\nRUN python -m pip install --upgrade " \
+                  "--trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org " \
+                  "pymongo==3.12.1 numpy\n"
+
+
+def test_combine_requirements():
+    ret = DockerManager.combine_requirements(base=['aaa', 'bbb==2.3', 'ccc'], additive=['bbb', 'ccc==2.5'])
+    assert sorted(ret) == sorted(['aaa', 'bbb', 'ccc==2.5'])
