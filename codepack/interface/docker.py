1d05e2c55acb035073dbf91208fb1afd4e182a92
ihnokim <ihnokim58@gmail.com>
2022-02-16 10:11:47 +0900

    Fix close

--- codepack/interface/docker.py
+++ codepack/interface/docker.py
@@ -4,20 +4,19 @@ from codepack.interface import Interface
 
 class Docker(Interface):
     def __init__(self, config=None, *args, **kwargs):
-        if not config:
-            config = dict()
-        super().__init__(config)
+        super().__init__(config if config else dict())
         self.connect(*args, **kwargs)
 
     def connect(self, *args, **kwargs):
         self.session = DockerClient(*args, **self.config, **kwargs)
-        self.closed = False
+        self._closed = False
         return self.session
 
     def __getattr__(self, item):
-        assert not self.closed, "connection is closed"
+        assert not self.closed(), "connection is closed"
         return getattr(self.session, item)
 
     def close(self):
         self.session.close()
-        self.closed = True
+        if not self.closed():
+            self._closed = True
