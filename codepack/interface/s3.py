8f5207fea5ef71b28e4df7ee144e77d505e82d57
ihnokim <ihnokim58@gmail.com>
2022-02-19 02:38:32 +0900

    Implement S3 interface

---
+++ codepack/interface/s3.py
@@ -0,0 +1,33 @@
+import boto3
+from botocore.config import Config
+from codepack.interface import Interface
+from typing import Union
+
+
+class S3(Interface):
+    def __init__(self, config: dict, *args, **kwargs):
+        super().__init__(config)
+        self.connect(*args, **kwargs)
+
+    def connect(self, *args, **kwargs):
+        self.session = boto3.client(config=Config(retries=dict(max_attempts=3)), *args, **self.config, **kwargs)
+        self._closed = False
+        return self.session
+
+    def list_objects(self, bucket: str, prefix: str = ''):
+        ret = list()
+        if self.session is None:
+            return ret
+        for page in self.session.get_paginator('list_objects').paginate(Bucket=bucket, Prefix=prefix):
+            ret += page.get('Contents', list())
+        return ret
+
+    def download(self, bucket: str, key: str):
+        obj = self.session.get_object(Bucket=bucket, Key=key)
+        return obj['Body'].read()
+
+    def upload(self, bucket: str, key: str, data: Union[str, bytes]):
+        self.session.put_object(Bucket=bucket, Key=key, Body=data)
+
+    def close(self):
+        self._closed = True
