8db381e0558ccb2820b7158eab8682538d3c3f6f
ihnokim <ihnokim58@gmail.com>
2022-02-20 22:54:43 +0900

    Implement delete

--- codepack/interface/s3.py
+++ codepack/interface/s3.py
@@ -14,20 +14,23 @@ class S3(Interface):
         self._closed = False
         return self.session
 
-    def list_objects(self, bucket: str, prefix: str = ''):
+    def list_objects(self, bucket: str, prefix: str = '', *args, **kwargs):
         ret = list()
         if self.session is None:
             return ret
-        for page in self.session.get_paginator('list_objects').paginate(Bucket=bucket, Prefix=prefix):
+        for page in self.session.get_paginator('list_objects').paginate(Bucket=bucket, Prefix=prefix, *args, **kwargs):
             ret += page.get('Contents', list())
         return ret
 
-    def download(self, bucket: str, key: str):
-        obj = self.session.get_object(Bucket=bucket, Key=key)
+    def download(self, bucket: str, key: str, *args, **kwargs):
+        obj = self.session.get_object(Bucket=bucket, Key=key, *args, **kwargs)
         return obj['Body'].read()
 
-    def upload(self, bucket: str, key: str, data: Union[str, bytes]):
-        self.session.put_object(Bucket=bucket, Key=key, Body=data)
+    def delete(self, bucket: str, key: str, *args, **kwargs):
+        return self.session.delete_object(Bucket=bucket, Key=key, *args, **kwargs)
+
+    def upload(self, bucket: str, key: str, data: Union[str, bytes], *args, **kwargs):
+        return self.session.put_object(Bucket=bucket, Key=key, Body=data, *args, **kwargs)
 
     def close(self):
         self._closed = True
