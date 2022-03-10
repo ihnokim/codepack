f731d05197d2752911579a86fef99876697d1bce
ihnokim <ihnokim58@gmail.com>
2022-03-11 02:02:15 +0900

    Add InterpreterManager and CallbackService

--- codepack/employee/worker.py
+++ codepack/employee/worker.py
@@ -3,19 +3,24 @@ from codepack.snapshot import CodeSnapshot
 from codepack.employee.supervisor import Supervisor
 from codepack.storage import KafkaStorage
 from codepack.config import Default
-from codepack.docker import DockerManager
+from codepack.manager import DockerManager, InterpreterManager
+from codepack.service import CallbackService
 from codepack.callback.functions import inform_supervisor_of_termination
 from functools import partial
 import os
 
 
 class Worker(KafkaStorage):
-    def __init__(self, consumer=None, interval=1, script='run_snapshot.py', callback=None,
-                 supervisor=None, docker_manager=None, consumer_config=None):
+    def __init__(self, consumer=None, interval=1, path=None, script='run_snapshot.py', callback=None,
+                 supervisor=None, docker_manager=None, interpreter_manager=None,
+                 callback_service=None, consumer_config=None):
         KafkaStorage.__init__(self, consumer=consumer, consumer_config=consumer_config)
         self.interval = interval
         self.supervisor = supervisor
         self.docker_manager = None
+        self.interpreter_manager = None
+        self.callback_service = None
+        self.path = path
         self.script = script
         self.callback = callback
         if self.supervisor and not self.callback:
@@ -25,6 +30,8 @@ class Worker(KafkaStorage):
                 self.close()
                 raise TypeError(type(self.supervisor))
         self.init_docker_manager(docker_manager=docker_manager)
+        self.init_interpreter_manager(interpreter_manager=interpreter_manager)
+        self.init_callback_service(callback_service=callback_service)
 
     def init_docker_manager(self, docker_manager):
         if docker_manager is None:
@@ -33,6 +40,22 @@ class Worker(KafkaStorage):
             self.docker_manager = docker_manager
         else:
             raise TypeError(type(docker_manager))
+
+    def init_interpreter_manager(self, interpreter_manager):
+        if interpreter_manager is None:
+            self.interpreter_manager = Default.get_interpreter_manager()
+        elif isinstance(interpreter_manager, InterpreterManager):
+            self.interpreter_manager = interpreter_manager
+        else:
+            raise TypeError(type(interpreter_manager))
+
+    def init_callback_service(self, callback_service):
+        if callback_service is None:
+            self.callback_service = Default.get_service('callback', 'callback_service')
+        elif isinstance(callback_service, CallbackService):
+            self.callback_service = callback_service
+        else:
+            raise TypeError(type(callback_service))
 
     def start(self):
         self.consumer.consume(self.work, timeout_ms=int(float(self.interval) * 1000))
@@ -51,16 +74,23 @@ class Worker(KafkaStorage):
         code = Code.from_snapshot(snapshot)
         code.register_callback(callback=self.callback)
         try:
-            if code.image:
+            if code.image or code.env:
                 state = code.check_dependency()
+                cb_id = self.callback_service.push(self.callback)
                 code.update_state(state, args=snapshot.args, kwargs=snapshot.kwargs)
                 if state == 'READY':
                     filepath = '%s.json' % code.serial_number
-                    full_filepath = os.path.join(self.docker_manager.path, filepath)
-                    snapshot.to_file(full_filepath)
-                    ret = self.docker_manager.run(image=code.image, command=['python', self.script, filepath])
-                    print(ret.decode('utf-8').strip())
-                code.run_callback()
+                    if code.env:
+                        full_filepath = os.path.join(self.path, filepath)
+                        snapshot.to_file(full_filepath)
+                        self.interpreter_manager.run(env=code.env, command=['python', self.script, full_filepath, '-c', cb_id])
+                    else:  # if code.image:
+                        full_filepath = os.path.join(self.docker_manager.path, filepath)
+                        snapshot.to_file(full_filepath)
+                        ret = self.docker_manager.run(image=code.image,
+                                                      command=['python', self.script, filepath, '-c', cb_id],
+                                                      path=self.docker_manager.path)
+                        print(ret.decode('utf-8').strip())
             else:
                 code(*snapshot.args, **snapshot.kwargs)
         except Exception as e:
