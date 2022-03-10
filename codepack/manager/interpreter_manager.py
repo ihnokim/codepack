e6cda0ca721ef5c2646fe3bc6ac13c6feb4bd4f0
ihnokim <ihnokim58@gmail.com>
2022-03-10 23:48:36 +0900

    Implement InterpreterManager

---
+++ codepack/manager/interpreter_manager.py
@@ -0,0 +1,25 @@
+from codepack.manager.manager import Manager
+import os
+from typing import Union
+import json
+import subprocess
+
+
+class InterpreterManager(Manager):
+    def __init__(self, path: str, run_opt: str = None):
+        self.path = path
+        if run_opt:
+            self.run_opt = json.loads(run_opt)
+        else:
+            self.run_opt = dict()
+
+    def get_env(self, env: str):
+        return os.path.join(self.path, env, 'bin')
+
+    def run(self, env: str, command: Union[str, list] = None, **kwargs):
+        if len(command) > 0 and 'python' in command[0]:
+            interpreter = os.path.join(self.get_env(env), command[0])
+            _command = [interpreter, *command[1:]]
+            return subprocess.run(_command, **kwargs, **self.run_opt)
+        else:
+            raise ValueError('invalid command: %s' % command)
