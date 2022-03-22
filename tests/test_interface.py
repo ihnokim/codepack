20900a71492e239c7a7815db20aa9cf9647b706e
ihnokim <ihnokim58@gmail.com>
2022-03-23 02:07:08 +0900

    Add test cases for MySQL

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -153,3 +153,58 @@ def test_mysql_select(mock_client):
         m.select(db='codepack', table='table1', projection=['c1', 'c2', 'c3'],
                  test_key1='test_value1', test_key2=1.23, test_key3=123)
     mock_client().rollback.assert_called_once()
+
+
+@patch('pymysql.connect')
+def test_mysql_insert(mock_client):
+    mysql_config = {'host': 'localhost', 'port': 3306, 'user': 'admin', 'passwd': 'test', 'charset': 'utf8',
+                    'cursorclass': 'pymysql.cursors.DictCursor'}
+    m = MySQL(config=mysql_config)
+    m.insert(db='codepack', table='table1',
+             test_key1='test_value1', test_key2=1.23, test_key3=123)
+    mock_client().cursor.assert_called_once()
+    mock_client().cursor().execute.assert_called_once_with("replace into "
+                                                           "codepack.table1 (test_key1, test_key2, test_key3) "
+                                                           "values ('test_value1', 1.23, 123)")
+    mock_client().cursor().fetchall.assert_called_once()
+    mock_client().cursor().close.assert_called_once()
+    mock_client().commit.assert_not_called()
+    mock_client().rollback.assert_not_called()
+    m.insert(db='codepack', table='table1',
+             test_key1='test_value1', test_key2=1.23, test_key3=123, update=False)
+    mock_client().cursor().execute.assert_called_with("insert into "
+                                                      "codepack.table1 (test_key1, test_key2, test_key3) "
+                                                      "values ('test_value1', 1.23, 123)")
+    assert mock_client().cursor().fetchall.call_count == 2
+    assert mock_client().cursor().close.call_count == 2
+    mock_client().commit.assert_not_called()
+    mock_client().rollback.assert_not_called()
+    m.insert(db='codepack', table='table1',
+             test_key1='test_value1', test_key2=1.23, test_key3=123, update=False, commit=True)
+    mock_client().commit.assert_called_once()
+    mock_client().cursor.return_value = None
+    with pytest.raises(AttributeError):
+        m.insert(db='codepack', table='table1',
+                 test_key1='test_value1', test_key2=1.23, test_key3=123)
+    mock_client().rollback.assert_called_once()
+
+
+@patch('pymysql.connect')
+def test_mysql_insert_many(mock_client):
+    mysql_config = {'host': 'localhost', 'port': 3306, 'user': 'admin', 'passwd': 'test', 'charset': 'utf8',
+                    'cursorclass': 'pymysql.cursors.DictCursor'}
+    m = MySQL(config=mysql_config)
+    rows = [{'test_key1': 'test_value1', 'test_key2': 1.23, 'test_key3': 123},
+            {'test_key1': 'test_value2', 'test_key2': 2.34, 'test_key3': 234},
+            {'test_key1': 'test_value3', 'test_key2': 3.45, 'test_key3': 345}]
+    m.insert_many(db='codepack', table='table1', rows=rows)
+    mock_client().cursor.assert_called_once()
+    assert mock_client().cursor().execute.call_count == 3
+    mock_client().cursor().fetchall.assert_called_once()
+    mock_client().cursor().close.assert_called_once()
+    mock_client().commit.assert_not_called()
+    mock_client().rollback.assert_not_called()
+    mock_client().cursor.return_value = None
+    with pytest.raises(AttributeError):
+        m.insert_many(db='codepack', table='table1', rows=rows)
+    mock_client().rollback.assert_called_once()
