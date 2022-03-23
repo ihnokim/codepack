cacbd62a45b83a5b41215e559c52f3cdf93d2ef9
ihnokim <ihnokim58@gmail.com>
2022-03-24 02:33:20 +0900

    Add MSSQL tests for init and select

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,5 +1,5 @@
 from unittest.mock import patch
-from codepack.interface import DynamoDB, MySQL
+from codepack.interface import DynamoDB, MySQL, MSSQL
 import pytest
 from botocore.config import Config
 from decimal import Decimal
@@ -255,4 +255,92 @@ def test_mysql_call(mock_client):
     mock_client().cursor.return_value = None
     with pytest.raises(AttributeError):
         m.call(db='codepack', procedure='test_procedure', test_key1='test_value1', test_key2=1.23, test_key3=123)
+    mock_client().rollback.assert_called_once()
+
+
+@patch('pymssql.connect')
+def test_mssql_init(mock_client):
+    mssql_config = {'host': 'localhost', 'port': 3306, 'user': 'admin', 'password': 'test', 'charset': 'utf8',
+                    'as_dict': 'False'}
+    m = MSSQL(config=mssql_config)
+    arg_list = mock_client.call_args_list
+    assert len(arg_list) == 1
+    args, kwargs = arg_list[0]
+    assert kwargs.get('host', '') == 'localhost'
+    assert kwargs.get('port', '') == 3306
+    assert kwargs.get('user', '') == 'admin'
+    assert kwargs.get('password', '') == 'test'
+    assert kwargs.get('charset', '') == 'utf8'
+    assert 'as_dict' in kwargs and isinstance(kwargs['as_dict'], bool)
+    assert not kwargs.get('as_dict', True)
+    assert m.session is mock_client()
+    m.close()
+    mock_client().close.assert_called_once()
+    assert m.closed()
+
+
+@patch('sshtunnel.SSHTunnelForwarder')
+@patch('pymssql.connect')
+def test_mssql_init_with_sshtunnel_from_config_file(mock_client, mock_ssh):
+    mssql_config = {'host': 'localhost', 'port': 3306, 'user': 'admin', 'password': 'test', 'charset': 'utf8',
+                    'as_dict': 'True', 'sshtunnel': 'config/test_conn.ini:ssh'}
+    m = MSSQL(config=mssql_config)
+    assert m.ssh_config == {'ssh_host': 'localhost', 'ssh_port': '22', 'ssh_username': 'test', 'ssh_password': '1234'}
+    mock_ssh.assert_called_once_with(('localhost', 22), remote_bind_address=('localhost', 3306),
+                                     ssh_password='1234', ssh_username='test')
+    arg_list = mock_client.call_args_list
+    assert len(arg_list) == 1
+    args, kwargs = arg_list[0]
+    assert kwargs.get('host', '') == '127.0.0.1'
+    assert kwargs.get('port', '') != 3306
+    assert kwargs.get('user', '') == 'admin'
+    assert kwargs.get('password', '') == 'test'
+    assert kwargs.get('charset', '') == 'utf8'
+    assert 'as_dict' in kwargs and isinstance(kwargs['as_dict'], bool)
+    assert kwargs.get('as_dict', False)
+    assert m.session is mock_client()
+    m.close()
+    mock_client().close.assert_called_once()
+    mock_ssh().stop.assert_called_once()
+    assert m.closed()
+
+
+@patch('sshtunnel.SSHTunnelForwarder')
+@patch('pymssql.connect')
+def test_mssql_init_with_sshtunnel_from_dict(mock_client, mock_ssh):
+    mssql_config = {'host': 'localhost', 'port': 3306, 'user': 'admin', 'password': 'test', 'charset': 'utf8',
+                    'as_dict': 'False',
+                    'sshtunnel': {'ssh_host': 'localhost', 'ssh_port': '22',
+                                  'ssh_username': 'test', 'ssh_password': '1234'}}
+    m = MSSQL(config=mssql_config)
+    assert m.ssh_config == {'ssh_host': 'localhost', 'ssh_password': '1234', 'ssh_port': '22', 'ssh_username': 'test'}
+    mock_ssh.assert_called_once_with(('localhost', 22), remote_bind_address=('localhost', 3306),
+                                     ssh_password='1234', ssh_username='test')
+    m.close()
+    mock_client().close.assert_called_once()
+    mock_ssh().stop.assert_called_once()
+    assert m.closed()
+
+
+@patch('pymssql.connect')
+def test_mssql_select(mock_client):
+    mssql_config = {'host': 'localhost', 'port': 3306, 'user': 'admin', 'password': 'test', 'charset': 'utf8',
+                    'as_dict': 'False'}
+    m = MSSQL(config=mssql_config)
+    mock_client().cursor.return_value.rowcount = -1
+    m.select(db='codepack', table='table1', projection=['c1', 'c2', 'c3'],
+             test_key1='test_value1', test_key2=1.23, test_key3=123, test_key4=['a', 'b', 'c'])
+    mock_client().cursor.assert_called_once()
+    mock_client().cursor().execute.assert_called_once_with("select c1,c2,c3 from codepack.table1 "
+                                                           "where test_key1 = 'test_value1' "
+                                                           "and test_key2 = 1.23 and test_key3 = 123 "
+                                                           "and test_key4 in ('a', 'b', 'c')")
+    mock_client().cursor().fetchall.assert_called_once()
+    mock_client().cursor().close.assert_called_once()
+    mock_client().commit.assert_not_called()
+    mock_client().rollback.assert_not_called()
+    mock_client().cursor.return_value = None
+    with pytest.raises(AttributeError):
+        m.select(db='codepack', table='table1', projection=['c1', 'c2', 'c3'],
+                 test_key1='test_value1', test_key2=1.23, test_key3=123, test_key4=['a', 'b', 'c'])
     mock_client().rollback.assert_called_once()
