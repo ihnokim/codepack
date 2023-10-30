6243e07381a455c8c4cc8b502fbfa291038b20f7
ihnokim <ihnokim58@gmail.com>
2023-10-30 22:29:06 +0900

    Remove deprecated classes

--- codepack/plugins/manager.py
+++
@@ -1,8 +0,0 @@
-import abc
-from typing import Union, Any
-
-
-class Manager(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
-    def run(self, command: Union[str, list], **kwargs: Any) -> Any:
-        """run with given command"""
